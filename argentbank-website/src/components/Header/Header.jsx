aa3c0d835b1f42d10d4a1d795b72baa28997908c
Tomhashcat <dutourne.t@gmail.com>
2023-11-21 12:54:02 +0100

    :fix header icone

--- argentbank-website/src/components/Header/Header.jsx
+++ argentbank-website/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 import logoImg from "../../assets/img/argentBankLogo.png";
 
@@ -8,25 +8,29 @@ import logoImg from "../../assets/img/argentBankLogo.png";
  * @returns {React.ReactElement} JSX.Element - header component
  */
 function Header() {
-    
-   
-    return (
+  return (
+    <>
+      <link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+      
+       
 
-    <header className="main-nav">
+      <header className="main-nav">
         <Link className="main-navo-log" to="/">
-            <img
-                className="main-nav-logo-image"
-                src={logoImg}
-                alt="Argent Bank Logo"
-            />
-            <h1 className="sr-only">Argent Bank</h1>
+          <img
+            className="main-nav-logo-image"
+            src={logoImg}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
         </Link>
         <div>
-            <Link to="/Login" className="main-nav-item" ><i className="fa fa-user-circle"></i>Sign in</Link>
+          <Link to="/Login" className="main-nav-item">
+            <i className="fa fa-user-circle"></i>Sign in
+          </Link>
         </div>
-    </header>
-);
-
+      </header>
+    </>
+  );
 }
 
 export default Header;
