f0fc1ad0947c773387040340bcf5823e8f515bdd
Tomhashcat <dutourne.t@gmail.com>
2023-12-01 17:09:00 +0100

    :fix home

--- argentbank-website/src/pages/Home/Home.jsx
+++ argentbank-website/src/pages/Home/Home.jsx
@@ -56,51 +56,52 @@ function Home(){
 	return (
 		
 		
-		 <>
-     <main className="main bg-dark">
-        <section className="sign-in-content">
-          <i className="fa fa-user-circle sign-in-icon"></i>
-          <h1>Sign In</h1>
-          <form onSubmit={handelSubmit}>
-            <div className="input-wrapper">
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                id="username"
-                name="email"
-                onChange={handelChange}
-              />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                onChange={handelChange}
-              />
-            </div>
-            <div className="input-remember">
-              <input
-                type="checkbox"
-                id="remember-me"
-                name="remember-me"
-                defaultChecked={isRemember}
-                onChange={() => setIsRemember(!isRemember)}
-              />
-              <label htmlFor="remember-me">Remember me</label>
-            </div>
-            <button type="submit" variant="success" className="sign-in-button">
-              Sign In
-            </button>
-           
-          </form>
+    <>
+      <div className="hero">
+        <section className="hero-content">
+          <h2 className="sr-only">Promoted Content</h2>
+          <p className="subtitle">No fees.</p>
+          <p className="subtitle">No minimum deposit.</p>
+          <p className="subtitle">High interest rates.</p>
+          <p className="text">Open a savings account with Argent Bank today!</p>
         </section>
-      </main>
-    </>
-	
-            
-	);
-
-	}
+      </div>
+      <section className="features">
+        <h2 className="sr-only">Features</h2>
+        <div className="feature-item">
+          <img src={iconChat} alt="Chat Icon" className="feature-icon" />
+          <h3 className="feature-item-title">You are our #1 priority</h3>
+          <p>
+            Need to talk to a representative? You can get in touch through our
+            24/7 chat or through a phone call in less than 5 minutes.
+          </p>
+        </div>
+        <div className="feature-item">
+          <img
+            src={inconMoney}
+            alt="Chat Icon"
+            className="feature-icon"
+          />
+          <h3 className="feature-item-title">More savings means higher rates</h3>
+          <p>
+            The more you save with us, the higher your interest rate will be!
+          </p>
+        </div>
+        <div className="feature-item">
+          <img
+            src={iconSecurity}
+            alt="Chat Icon"
+            className="feature-icon"
+          />
+          <h3 className="feature-item-title">Security you can trust</h3>
+          <p>
+            We use top of the line encryption to make sure your data and money
+            is always safe.
+          </p>
+        </div>
+      </section>
+   
+   </>
+  );
+}
 export default Home;
