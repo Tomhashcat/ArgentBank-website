21af580ac75d84dc1f3c7e770d0c50eb178e371a
Tomhashcat <dutourne.t@gmail.com>
2023-12-06 18:17:00 +0100

    :fix

--- argentbank-website/src/store.js
+++ argentbank-website/src/store.js
@@ -3,11 +3,11 @@ import logger from 'redux-logger'
 import profileReducer from './pages/Users/profileSlice';
 
 const store = configureStore({
-  reducer: 
-    profileReducer,
+  reducer: {
+    profile: profileReducer,
     // Ajoutez d'autres réducteurs si nécessaire
-  
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
